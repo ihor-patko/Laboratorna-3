46c3988cbe5a6d90cfc026f26df3819544cab55b
ihor-patko <91693589+ihor-patko@users.noreply.github.com>
2021-10-31 18:17:35 +0200

    Update lab3.c

--- lab3.c
+++ lab3.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void function(){
+void func(){
      int a,b;
      int * pA = &a;
      int * pB = &b;
@@ -20,7 +20,7 @@ void function(){
          for (int i = 1 ; *pA <= *pB ; *pA += i)
          {
              arr[*pA]= *pA;
-             if ((*pA % 10 != 5) & (*pA % 100 < 50 || *pA % 100 > 59)
+             if ((*pA % 10 != 5) & (*pA % 100 < 50 || *pA % 100 > 59)//Checking the presence of the number 5 in the entered number a.
              & (*pA % 1000 < 500 || *pA % 1000 > 599)
              & (*pA % 10000 < 5000 || *pA % 10000 > 5999)
              & (*pA % 100000 < 50000 || *pA % 100000 > 59999)
@@ -38,5 +38,5 @@ void function(){
 
 int main() 
 {
-    function();
+    func();
 }
